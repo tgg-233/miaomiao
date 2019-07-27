47699464c4d9b3e4e21c8a80d9c2c0899c88817c
China BOSS <1049042122@qq.com>
2019-07-27 16:34:03 +0800

    add createComponents

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from '@/App'
+import router from 'router'
+
+import '../static/css/common.css'
+import '../static/css/iconfont/iconfont.css'
 
 Vue.config.productionTip = false
 
@@ -12,4 +15,5 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+  // render: (c) => c(App)
 })
---
+++ src/router/movie/index.js
@@ -0,0 +1,27 @@
+// 按需加载
+export default {
+  path: '/movie',
+  component: () => import('views/Movie'),
+  children: [
+    {
+      path: 'city',
+      component: () => import('components/City')
+    },
+    {
+      path: 'nowPlaying',
+      component: () => import('components/NowPlaying')
+    },
+    {
+      path: 'comingSoon',
+      component: () => import('components/Comingsoon')
+    },
+    {
+      path: 'search',
+      component: () => import('components/Search')
+    },
+    {
+      path: '/movie',
+      redirect: '/movie/nowPlaying'
+    }
+  ]
+}
